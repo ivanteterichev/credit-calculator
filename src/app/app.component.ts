bab00da3dd60eaf1e87a6693e50c1f9a4ea97b9b
Ivan Teterichev <i.teterichev@jumpnet.ru>
2020-06-04 18:13:22 +0500

    Добавление логики для первоначального взноса в зависимости от цены

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CreditCalculator';
+  carPrice = 1970000;
+  minDeposit = 197000;
+  carPrices = [1970000, 2370000, 3670000, 1970000, 2370000, 3670000];
+  outputPrice(price) {
+    console.log(price);
+    this.carPrice = price;
+    this.minDeposit = price * .1;
+  }
 }
