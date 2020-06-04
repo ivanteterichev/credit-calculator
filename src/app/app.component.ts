72cbd8329b5be03180899ed2d1d2f2903eb5d75f
Ivan Teterichev <i.teterichev@jumpnet.ru>
2020-06-04 23:51:39 +0500

    Добавление логики расчета ежемесячного платежа

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,17 @@ export class AppComponent {
   title = 'CreditCalculator';
   carPrice = 1970000;
   minDeposit = 197000;
+  minPayment = 10000;
+  maxPayment = 50000;
+  period = 60;
   carPrices = [1970000, 2370000, 3670000, 1970000, 2370000, 3670000];
+  paymentPerMounth = null;
   outputPrice(price) {
     console.log(price);
     this.carPrice = price;
     this.minDeposit = price * .1;
+  };
+  calcCredit(carPrice, deposit, period) {
+    this.paymentPerMounth = (((((carPrice - deposit) / (period / 12)) * 1.118)) / 12).toFixed(0);
   }
 }
